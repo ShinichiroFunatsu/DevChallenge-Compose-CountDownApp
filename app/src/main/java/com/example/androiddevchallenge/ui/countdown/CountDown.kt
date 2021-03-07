e2f53b97022a4963b3c23503d6cc7ce9e43aca18
ShinichiroFunatsu <sfunatsu.git@gmail.com>
2021-03-07 12:21:23 +0900

    add control UIs.

--- app/src/main/java/com/example/androiddevchallenge/ui/countdown/CountDown.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/countdown/CountDown.kt
@@ -15,10 +15,21 @@
  */
 package com.example.androiddevchallenge.ui.countdown
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.Stop
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -28,6 +39,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.model.RawTime
 import com.example.androiddevchallenge.model.Time
 import kotlinx.coroutines.isActive
@@ -38,7 +50,7 @@ fun CountDown(
     modifier: Modifier = Modifier,
     onSetTimeClick: () -> Unit = {}
 ) {
-    val totalTime by remember { mutableStateOf(defaultRawTime.totalSeconds) }
+    var totalTime by remember { mutableStateOf(defaultRawTime.totalSeconds) }
     var startTime by remember { mutableStateOf(0L) }
     var timeLeft by remember { mutableStateOf(totalTime) }
     val seconds = remember { mutableStateOf(0) }
@@ -49,6 +61,7 @@ fun CountDown(
     LaunchedEffect(isRunning) {
         while (isActive && timeLeft > 0 && isRunning) {
             withFrameMillis {
+                if (!isRunning) return@withFrameMillis
                 if (startTime == 0L) {
                     startTime = it / 1000
                 }
@@ -66,12 +79,100 @@ fun CountDown(
 
     val time by remember { mutableStateOf(Time(hours, minutes, seconds)) }
 
-    ClockDial(time = time, modifier = modifier)
+    ClockDial(
+        time = time,
+        modifier = modifier,
+        isRunning = isRunning,
+        onStartClick = {
+            isRunning = true
+        },
+        onStopClick = {
+            isRunning = false
+            startTime = 0L
+            totalTime = timeLeft
+        },
+        onSetTimeClick = {
+            isRunning = false
+            onSetTimeClick()
+        }
+    )
 }
 
 @Composable
-fun ClockDial(time: Time, modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-        Text(text = "$time", modifier = Modifier.align(Alignment.Center), style = MaterialTheme.typography.h1)
+fun ClockDial(
+    time: Time,
+    modifier: Modifier = Modifier,
+    isRunning: Boolean,
+    onStartClick: () -> Unit,
+    onStopClick: () -> Unit,
+    onSetTimeClick: () -> Unit
+) {
+    Column(modifier = modifier) {
+        Box(
+            modifier = modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = "$time",
+                modifier = Modifier.align(Alignment.Center),
+                style = MaterialTheme.typography.h2
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(46.dp)
+                    .padding(horizontal = 12.dp)
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+                    .clickable(onClick = onSetTimeClick)
+            ) {
+                Text(
+                    text = "SET",
+                    modifier = Modifier.align(Alignment.Center),
+                    style = MaterialTheme.typography.h6
+                )
+            }
+            StartOrStopFab(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .align(Alignment.CenterVertically),
+                isRunning = isRunning,
+                onStopClick = onStopClick,
+                onStartClick = onStartClick
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun StartOrStopFab(
+    modifier: Modifier = Modifier,
+    isRunning: Boolean,
+    onStopClick: () -> Unit,
+    onStartClick: () -> Unit
+) {
+    FloatingActionButton(
+        modifier = modifier.padding(12.dp),
+        onClick = {
+            if (isRunning) {
+                onStopClick()
+            } else {
+                onStartClick()
+            }
+        }
+    ) {
+        Icon(
+            imageVector = if (isRunning) Icons.Outlined.Stop else Icons.Outlined.PlayArrow,
+            contentDescription = if (isRunning) "stop fab" else "start fab"
+        )
     }
 }
